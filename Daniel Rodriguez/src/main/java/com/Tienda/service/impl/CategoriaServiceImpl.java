d4263a2536d3f6a8a13face66024745470a55a64
Daniel24rv <drodriguez30182@ufide.ac.cr>
2024-02-22 14:15:01 -0600

    semana 6

---
+++ Daniel Rodriguez/src/main/java/com/Tienda/service/impl/CategoriaServiceImpl.java
@@ -0,0 +1,24 @@
+
+package com.Tienda.service.impl;
+import com.Tienda.Dao.CategoriaDao;
+import com.Tienda.domain.Categoria;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+    
+    @Autowired
+    private CategoriaDao categoriaDao;
+
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista=categoriaDao.findAll();
+        if (activos) {
+           lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
+    }
+}
--- Daniel Rodriguez/src/main/java/com/example/Tienda2/ProjectConfig.java
+++ Daniel Rodriguez/src/main/java/com/example/Tienda2/ProjectConfig.java
@@ -1,5 +1,6 @@
-package Tienda.Daniel_Rodriguez;
+package com.example.Tienda2;
 
+import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,21 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
-
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
 
+    // Método para crear una sesión de cambio de idioma
     @Bean
     public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.getDefault());
         slr.setLocaleAttributeName("session.current.locale");
         slr.setTimeZoneAttributeName("session.current.timezone");
-
         return slr;
     }
 
+    // Bean para interceptar el cambio de idioma
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         var lci = new LocaleChangeInterceptor();
@@ -32,14 +32,16 @@ public class ProjectConfig implements WebMvcConfigurer {
         return lci;
     }
 
+    // Método para agregar el interceptor al registro
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
 
+    // Bean para configurar el origen de los mensajes
     @Bean("messageSource")
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        var messageSource = new ResourceBundleMessageSource();
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
