86ad78bde70e5b68bffff6607610cb160758633b
Daniel24rv <134095766+Daniel24rv@users.noreply.github.com>
2024-02-27 20:14:21 -0600

    Add files via upload

---
+++ Tienda_Daniel_Rodriguez_VargasApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda_Daniel_Rodriguez_VargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
