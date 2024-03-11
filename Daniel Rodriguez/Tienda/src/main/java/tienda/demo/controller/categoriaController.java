44e3fb4e8bc6b1c49be3601d03c1685c111b9d45
Daniel24rv <drodriguez30182@ufide.ac.cr>
2024-03-10 22:38:16 -0600

    semana 7.1

--- Daniel Rodriguez/Tienda/src/main/java/tienda/demo/controller/categoriaController.java
+++ Daniel Rodriguez/Tienda/src/main/java/tienda/demo/controller/categoriaController.java
@@ -1,22 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tienda.demo.controller;
 
-import tienda.demo.service.categoriaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import tienda.demo.domain.Categoria;
+import tienda.demo.service.CategoriaService;
+import tienda.demo.service.impl.FirebaseStorageServiceImpl;
+
 @Controller
 @Slf4j
 @RequestMapping("/categoria")
-public class categoriaController {
+public class CategoriaController {
+    
     @Autowired
-    private categoriaService categoriaService;
+    private CategoriaService categoriaService;
 
     @GetMapping("/listado")
     public String inicio(Model model) {
@@ -25,4 +29,41 @@ public class categoriaController {
         model.addAttribute("totalCategorias", categorias.size());
         return "/categoria/listado";
     }
+    
+    @GetMapping("/nuevo")
+    public String categoriaNuevo(Categoria categoria) {
+        return "/categoria/modifica";
+    }
+
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            categoriaService.save(categoria);
+            categoria.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "categoria", 
+                            categoria.getIdCategoria()));
+        }
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaEliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
+    }
 }
+
--- Daniel Rodriguez/Tienda/src/main/java/tienda/demo/service/categoriaService.java
+++ Daniel Rodriguez/Tienda/src/main/java/tienda/demo/service/categoriaService.java
@@ -1,10 +1,21 @@
 
 package tienda.demo.service;
 
-import tienda.demo.domain.categoria;
 import java.util.List;
+import tienda.demo.domain.Categoria;
 
-public interface categoriaService {
+public interface CategoriaService {
+    
     // Se obtiene un listado de categorias en un List
-    public List<categoria> getCategorias(boolean activos);
+    public List<Categoria> getCategorias(boolean activos);
+    
+   // Se obtiene un Categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
 }
